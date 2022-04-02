96c1e79295c226f0b79aec436711801f79b19f9e
garebareDA <garedaiki@gmail.com>
2022-04-02 21:52:09 +0900

    add:parser

--- parser.go
+++ parser.go
@@ -1,2 +1,14 @@
 package main
 
+import (
+	"errors"
+	"strings"
+)
+
+func Parse(message string)([]string, error){
+	if message[0:1] == "!" {
+		return strings.Split(message[1:], " "), nil
+	} else {
+		return nil, errors.New("no es un command")
+	}
+}
