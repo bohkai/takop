6d9f4fac0e23beb39df67b7e1056ad6fbcd1d2b5
garebareDA <garedaiki@gmail.com>
2022-04-06 15:31:41 +0900

    fix:parser

--- parser.go
+++ parser.go
@@ -6,7 +6,11 @@ import (
 )
 
 func Parse(message string)([]string, error){
-	if message[0:1] == "!" {
+	if len(message) < 1 {
+		return nil, errors.New("message is empty")
+	}
+
+	if message[0:1] == "?" {
 		return strings.Split(message[1:], " "), nil
 	} else {
 		return nil, errors.New("no es un command")
