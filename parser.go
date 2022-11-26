db6367a3b152f45eecd371f95f851dd45713be85
garebareDA <garedaiki@gmail.com>
2022-11-26 14:35:04 +0900

    fix

--- parser.go
+++ parser.go
@@ -12,14 +12,15 @@ func Parse(message string)([]string, *string, error){
 
 	if message[0:1] == "?" {
 		split := strings.Split(message[1:], " ")
-		lastWard := split[len(split) - 1]
-		if len(lastWard) == 0 {
-			return nil, nil, nil
-		}
-
-		if lastWard[0:1] == "-" {
-			last := lastWard[1:]
-			return split, &last, nil
+		if len(split) - 1 > 0 {
+				lastWard := split[len(split) - 1]
+			if len(lastWard) == 0 {
+				return nil, nil, nil
+			}
+			if lastWard[0:1] == "-" {
+				last := lastWard[1:]
+				return split, &last, nil
+			}
 		}
 		return split, nil, nil
 	} else {
