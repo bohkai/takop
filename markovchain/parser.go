ae9ca0674d1bcf15aab85e100afc46c6ca2f8fc6
garebareDA <garedaiki@gmail.com>
2023-06-22 20:45:21 +0900

    feat:markov blocks

--- markovchain/parser.go
+++ markovchain/parser.go
@@ -31,3 +31,25 @@ func ParseToNode(m *mecab.MeCab, input string) ([]string, error) {
 
 	return words, nil
 }
+
+func GetMarkovBlocks(words []string) [][] string {
+	res := [][]string{}
+	if len(words) > 3 {
+		return res
+	}
+
+	resHead := []string{"#This is empty#", words[0], words[1]}
+	res = append(res, resHead)
+
+	for i := 1; i < len(words)-2; i++ {
+			markovBlock := []string{words[i], words[i+1], words[i+2]}
+			res = append(res, markovBlock)
+	}
+
+	resEnd := []string{words[len(words)-2], words[len(words)-1], "#This is empty#"}
+	res = append(res, resEnd)
+
+	return res
+}
+
+
